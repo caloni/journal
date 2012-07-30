2cfe7ea34c82e02eed9a0452c9e2514a07f93755
unknown <Caloni@Sonynha.(none)>
2012-07-29 22:37:12 -0300

    Exemplo de leitura de balanços da Bovespa pelo saite.

---
+++ BovespaReader/bovespa.py
@@ -0,0 +1,32 @@
+import urllib2, zipfile, StringIO, re
+
+def geturl(cvmcode, pubdata):
+    return 'http://www.bmfbovespa.com.br/dxw/Download.asp?moeda=L&site=B&mercado=18&ccvm=' + str(cvmcode) + '&data=' + pubdata + '&tipo=2'
+
+def getzip(url):
+    webRes = urllib2.urlopen(url)
+    doc = webRes.read()
+    return zipfile.ZipFile(StringIO.StringIO(doc))
+
+def opendoc(cvmcode, year):
+    """Carrega informacoes financeiras do site da Bovespa
+    
+    O cvmcode pode ser obtido no site da Bovespa (Empresas Listadas).
+    """
+    pubdata = '31/12/' + str(year)
+    url = geturl(cvmcode, pubdata)
+    try:
+        zip = getzip(url)
+        for name in ['DFPCDERE.001', 'WINDOWS/TEMP_CVM/DFPCDERE.001']:
+            if name in zip.namelist():
+                doc = zip.read(name)
+                break
+        else: raise KeyError
+        reglines = [re.match(r'^(.{6})(.{4})(.{2})(.{2})(.{13})(.{40})(.{15})(.{15})(.{15})(.{1})', x) for x in doc.splitlines()]
+        return (year, [[x.strip() for x in reglines[i].groups()] for i in range(len(reglines))])
+    except zipfile.BadZipfile:
+        print 'Zip invalido para ' + pubdata
+    except KeyError:
+        print 'Documento nao encontrado em ' + pubdata
+    return None
+
