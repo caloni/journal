513f42bc71af6abd0c3518157eb430c869047b31
Wanderley Caloni <wanderley@caloni.com.br>
2013-12-12 23:10:03 -0200

    Teste de linque.

--- OldPosts/sobrecarga1.cpp
+++ OldPosts/sobrecarga1.cpp
@@ -2,4 +2,5 @@ GUID guid;
 wstring guidS;
 
 CreateNewGUID(guidS); // chama void CreateNewGUID(wstring&)
-CreateNewGUID(guid); // chama void CreateNewGUID(GUID&) (o compilador sabe disso)// Codigo-fonte disponivel no GitHub (Caloni/Caloni.com.br) 
+CreateNewGUID(guid); // chama void CreateNewGUID(GUID&) (o compilador sabe disso)
+// Codigo-fonte disponivel no GitHub (<a href="https://github.com/Caloni/Caloni.com.br">Caloni/Caloni.com.br)</a>.
