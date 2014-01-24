fd5804c7dfe3eff2e80f9cd95dafbd5c3723ec84
Wanderley Caloni <wanderley@caloni.com.br>
2014-01-24 09:36:14 -0200

    Um filme muito, muito louco.

---
+++ MMs/sherlock.mm
@@ -0,0 +1,15 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1382357598974" ID="ID_1545735592" MODIFIED="1390391331276" TEXT="Sherlock">
+<node CREATED="1390391332001" ID="ID_947245749" MODIFIED="1390391335090" POSITION="right" TEXT="S03">
+<node CREATED="1390391336236" ID="ID_1296304273" MODIFIED="1390391337578" TEXT="E01">
+<node CREATED="1390391338553" ID="ID_905032039" MODIFIED="1390391347616" TEXT="Di&#xe1;logo com espectador">
+<node CREATED="1390391349465" ID="ID_58701156" MODIFIED="1390391366991" TEXT="&quot;Solu&#xe7;&#xe3;o decepcionante&quot;"/>
+<node CREATED="1390391401599" ID="ID_1089845404" MODIFIED="1390391406030" TEXT="Bomba">
+<node CREATED="1390391406584" ID="ID_621326082" MODIFIED="1390391410125" TEXT="Sempre tem um interruptor"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
