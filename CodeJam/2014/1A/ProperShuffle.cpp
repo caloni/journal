595354b7c21f3d40d03737f53ea634914764bc69
Wanderley Caloni <wanderley@caloni.com.br>
2014-05-03 12:45:49 -0300

    Fechando tentativas de resolver o 1A3.

---
+++ CodeJam/2014/1A/ProperShuffle.cpp
@@ -0,0 +1,14 @@
+#include "CodeJam.h"
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <set>
+#include <string>
+#include <numeric>
+
+using namespace std;
+
+
+void TestCase(std::istream& is, std::ostream& os)
+{
+}
