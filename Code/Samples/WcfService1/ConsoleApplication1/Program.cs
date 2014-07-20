d85aa14e1956bdb095781ab94f505033edfac7aa
Wanderley Caloni <wanderley@caloni.com.br>
2014-07-20 17:34:41 -0300

    Refatorando palestras e criando as três do TDC desse ano.

---
+++ Code/Samples/WcfService1/ConsoleApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WcfService1;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WcfService1.Service1 test = new WcfService1.Service1();
+            int z = test.Soma(3, 4);
+        }
+    }
+}
