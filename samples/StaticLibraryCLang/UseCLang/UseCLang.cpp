607f3946387588104b4c2ffd3d00d01c1cbdad1c
Wanderley Caloni <wanderley.caloni@bitforge.com.br>
2016-12-27 15:22:30 -0200

    One More Post

---
+++ samples/StaticLibraryCLang/UseCLang/UseCLang.cpp
@@ -0,0 +1,9 @@
+#include "..\StaticLibraryCLang\StaticLibraryCLang.h"
+
+#pragma comment(lib, "StaticLibraryCLang.lib")
+
+int main()
+{
+	int x = fnStaticLibraryCLang();
+	bool b = all(true, true, true, false);
+}
