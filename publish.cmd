3eb0f37b2daff3b0d42b7a1e042efe3bbe96cd82
Wanderley Caloni <wanderley.caloni@bitforge.com.br>
2018-08-13 22:55:29 -0300

    Publish apenas... publica, né.

--- publish.cmd
+++ publish.cmd
@@ -1,13 +1,7 @@
 @echo off
-pushd public
-rm -fr *
-popd
 hugo
 pushd public
 git add --all
-git commit --allow-empty %*
+git commit --allow-empty -m "Publishing site."
 git push
 popd
-git add --all
-git commit --allow-empty %*
-git push
