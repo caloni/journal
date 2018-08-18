fd238f260c45b3199cf8e52379ccdefe00a8ad07
Wanderley Caloni <wanderley.caloni@bitforge.com.br>
2018-08-18 15:44:51 -0300

    Publicacao incremental novamente (sem paginação e categorias ficou minúsculo).

--- publish.cmd
+++ publish.cmd
@@ -1,11 +1,8 @@
 @echo off
-rm -fr public
 hugo
 pushd public
-git init
 git add --all
 git commit -m "Publishing."
-git remote add origin https://github.com/cinetenisverde/cinetenisverde.github.io
-git push --force -u origin master
+git push
 popd
 git push
