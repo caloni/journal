0eea60f69dd49514d731574786f30d52e730e19e
Wanderley Caloni <wanderley.caloni@bitforge.com.br>
2019-01-01 22:28:45 -0200

    Adicionando script para publicação do livro Kindle (em meu Kindle) e ignorando pasta onde ele é gerado.

---
+++ publish-kindle.cmd
@@ -0,0 +1,7 @@
+rem @echo off
+hugo --theme book --destination book
+pushd book
+iconv -f UTF-8 -t LATIN1 index.html > book.html
+kindlegen.exe book.opf -o cinetenisverde.mobi
+if exist k:\ copy /y cinetenisverde.mobi k:\documents
+popd
