39fb04401f00789e7d68b942fd861d01e410b5f1
Wanderley Caloni <wanderley.caloni@bitforge.com.br>
2019-01-29 13:20:14 -0200

    A Era do Rádio é um Woody Allen contemplativo sobre sua própria infância.

---
+++ b.bat
@@ -0,0 +1,2 @@
+call share.cmd fa740cf75c8817ea246aced080c3947be2c31fb0 https://m.media-amazon.com/images/M/MV5BMmFmNWMxOGEtZmUwNS00NGEzLWI0ZmMtMzcwY2EzMzlhMDUwXkEyXkFqcGdeQXVyNTI4MjkxMDE@._V1_.jpg
+    rem Mens@gem Para Você ironicamente é comédia romântica ironicamente sobre a perda do romantismo com a vinda da internet e das megastores.
