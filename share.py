5376b90fdc0c9ead05a152ba28ec98b686e53d7e
Wanderley Caloni <wanderley.caloni@bitforge.com.br>
2019-03-17 13:48:24 -0300

    Recauchutada no script de share (e eliminando os testes).

--- share.py
+++ share.py
@@ -4,68 +4,33 @@ import sys
 import re
 import shutil
 import subprocess
-import urllib2
+import urllib
+from urllib.request import urlopen
 import time
 import glob
 from PIL import Image
 import os
 import webbrowser
-import msvcrt as m
-from pyshorteners import Shortener
-import frontmatter
 from bs4 import BeautifulSoup
 from markdown import markdown
 import git
 
 sys.path.append(r'c:\users\caloni\.pwd')
 import twitter_cinetenisverde as twitter_credentials
-import facebook_cinetenisverde as facebook_credentials
-import adorocinema_cinetenisverde as adorocinema_credentials
-
 
 baseUrl = 'http://cinetenisverde.com.br/' 
 
 
-
-def WebPageExists(url):
-    url = url + '/'
-    print '*** Waiting page ' + url
-    try:
-        urllib2.urlopen(url)
-    except urllib2.HTTPError as e:
-        print 'HTTPError', e.reason
-        return False
-    except urllib2.URLError as e:
-        print 'URLError'
-        return False
-    print 'that is ok'
-    return True
-
-
 def PrintStars(stars):
     ret = ''
-    starFull = unichr(0x2605)
-    starEmpty = unichr(0x2606)
+    starFull = chr(0x2605)
+    starEmpty = chr(0x2606)
     for s in range(0, int(stars)):
         ret = ret + starFull
     for s in range(int(stars), 5):
         ret = ret + starEmpty
     ret = ret + ' '
-    return ret.encode('utf-8')
-
-
-def GetSentence(postInfo, remaining):
-    sentence = postInfo['paragraph']
-    sentenceEnd = sentence.find('.')
-    if sentenceEnd == -1:
-        sentenceEnd = min(remaining, len(s))
-    sentenceEnd = min(sentenceEnd, remaining)
-    sentence = sentence[0:sentenceEnd+1]
-    if sentence[-1] == ' ':
-        sentence = sentence[0:-1] + '...'
-    elif sentence[-1] != '.':
-        sentence = sentence + '...'
-    return sentence.decode('utf8')
+    return ret
 
 
 def PublishToTwitter(postInfo, img):
@@ -77,88 +42,51 @@ def PublishToTwitter(postInfo, img):
     t_up = twitter.Twitter(domain='upload.twitter.com', auth=twitter_credentials.auth)
     id_img1 = t_up.media.upload(media=img)["media_id_string"]
 
-    stars = PrintStars(postInfo['stars']) if postInfo.has_key('stars') else ''
-    remaining = 130 - len(stars.decode('utf8') + ' ' + postInfo['subtitle'].decode('utf8') + '\n' + '\n' + postInfo['shortlink'])
-    st = stars.decode('utf8') + ' ' + postInfo['subtitle'].decode('utf8') + '\n' + postInfo['shortlink']
+    stars = PrintStars(postInfo['stars']) if 'stars' in postInfo else ''
+    remaining = 130 - len(stars + ' ' + postInfo['subtitle'] + '\n' + '\n' + postInfo['shortlink'])
+    st = stars + ' ' + postInfo['subtitle'] + '\n' + postInfo['shortlink']
     t.statuses.update(status=st, media_ids=",".join([id_img1]))
 
 
-def MakeSharePost(postInfo):
-    st = postInfo['subtitle'].decode('utf8') + '\n\n' + postInfo['desc'].decode('utf8') + '\n\n' + postInfo['paragraph'].decode('utf8') + '\n\n' + postInfo['link'].decode('utf8')
-    st2 = ''
-    if postInfo.has_key('cabine'):
-        st = st + '\n\n' + 'Em breve crítica completa no www.cinemaqui.com.br.'.decode('utf8')
-        st2 = '\nOu se você é membro do Artxs Humanxs basta clicar aqui ;) https://github.com/Caloni/cinetenisverde/tree/master/content/cinemaqui/'.decode('utf8') + postInfo['permalink'] + '.md'
-    return st, st2
-
-def PublishToFacebook(postInfo, st, img):
-    """
-    http://nodotcom.org/python-facebook-tutorial.html
-    """
-    stars = PrintStars(postInfo['stars']) if postInfo.has_key('stars') else ''
-    st = stars.decode('utf8') + ' ' + st
-    post = facebook_credentials.auth.put_photo(image=img, message=st)
-
-def SearchAdoroCinema(postInfo):
-    titleSearch = postInfo['title'].decode('utf8')
-    titleSearch = titleSearch.replace(' ', '+')
-    webbrowser.open_new_tab('http://www.adorocinema.com/busca/?q=' + titleSearch)
+def GetPermalinkFromCommit(ref):
+	g = git.Repo('.')
+	ci = g.commit(ref)
+	for f, d in ci.stats.files.items():
+		if f.find('content') != -1:
+			return f[f.rfind('/')+1:f.rfind('.')]
 
 
-def TreatParagraph(p):
-    p = p.decode('utf8')
-    p = unicode(p)
-    html = markdown(p)
-    text = ''.join(BeautifulSoup(html, 'lxml').findAll(text=True))
-    text = text.encode('utf8')
-    return text
+def GetCommentFromCommit(ref):
+	g = git.Repo('.')
+	ci = g.commit(ref)
+	return ci.message
 
 
-def GetPostInfo(post):
+def GetPostInfo(ref):
+    post = GetPermalinkFromCommit(ref) 
     postInfo = { 'file' : 'content\\post\\' + post + '.md' }
     postInfo['permalink'] =  post
     postInfo['category'] = 'movies'
-    f = open(postInfo['file'])
-    endHeader = 0
-    firstParagraph = False
-    for l in f.readlines():
-        if endHeader == 2:
-            m = re.search('^([^.]*.)', l)
-            if m:
-                postInfo['tagline'] = m.group(1)
-                postInfo['paragraph'] = TreatParagraph(l)
-                endHeader = 0
-        if l == '---\n':
-            endHeader += 1
-            pass
-        m = re.match('^title: \"(.*)\"', l)
-        if m:
-            postInfo['title'] = m.group(1)
-        m = re.match('^stars: \"([1-5])/5\"', l)
-        if m:
-            postInfo['stars'] = m.group(1)
-        m = re.match('^category: \"(.*)\"', l)
-        if m:
-            postInfo['category'] = m.group(1)
-        m = re.match('^cabine: \"(.*)\"', l)
-        if m:
-            postInfo['cabine'] = True
-        m = re.match('^desc: \"(.*)\"', l)
-        if m:
-            postInfo['desc'] = m.group(1)
-        m = re.match('^imdb: \"(.*)\"', l)
-        if m:
-            postInfo['imdb'] = m.group(1)
+    postInfo['link'] = baseUrl + postInfo['permalink'] + '/index.html'
+    postInfo['shortlink'] = postInfo['link']
+    comment = GetCommentFromCommit(ref)
+    postInfo['subtitle'] = comment
+
+    infos = {
+        'title': '^title: \"(.*)\"',
+        'stars': '^stars: \"([1-5])/5\"',
+        'category': '^category: \"(.*)\"',
+        'cabine': '^cabine: \"(.*)\"',
+        'desc': '^desc: \"(.*)\"',
+        'imdb': '^imdb: \"(.*)\"',
+    }
+    file = open(postInfo['file'])
+    for line in file.readlines():
+        for key, search in infos.items():
+            match = re.match(search, line)
+            if match:
+                postInfo[key] = match.group(1)
     return postInfo
-
-
-def PushChanges(postInfo):
-    subprocess.call(['git', 'add', postInfo['category'] + '/' + postInfo['file']])
-    subprocess.call(['git', 'add', '..\\images\\screenshots\\' + postInfo['permalink'] + '.jpg'])
-    #todo: fix encoding thing
-    #subprocess.call(['git', 'commit', '-m', '\"' + postInfo['tagline'].encode(sys.stdout.encoding) + '\"'])
-    subprocess.call(['git', 'commit', '-m', 'Movie Review'])
-    subprocess.call(['git', 'push'])
 
 
 def FindPostImageAndPrepare(postInfo):
@@ -175,68 +103,45 @@ def FindPostImageAndPrepare(postInfo):
         if origPath != newPath:
             os.remove(origPath)
 
-def GetPermalinkFromCommit(ref):
-	g = git.Repo('.')
-	ci = g.commit(ref)
-	for f, d in ci.stats.files.iteritems():
-		if f.find('content') != -1:
-			return f[f.rfind('/')+1:f.rfind('.')]
 
-
-def GetCommentFromCommit(ref):
-	g = git.Repo('.')
-	ci = g.commit(ref)
-	return ci.message
+def WebPageExists(url):
+    try:
+        urlopen(url)
+        return True
+    except urllib.error.HTTPError as e:
+        print(e)
+        return False
 
 
 def PublishToSocialMedia(ref, img):
-    post = GetPermalinkFromCommit(ref) 
-    print 'Publishing ' + post + '...'
-    afterMove = False
-    republish = False
     try:
-        print '*** Getting post info'
-        postInfo = GetPostInfo(post)
-        link = baseUrl + postInfo['permalink']
+        print('getting post info')
+        postInfo = GetPostInfo(ref)
+        print('waiting for page ' + postInfo['link'], end='')
+        sys.stdout.flush()
+        while not WebPageExists(postInfo['link']):
+            sys.stdout.write('.') 
+            sys.stdout.flush()
+            time.sleep(1)
+        print('')
 
-        while WebPageExists(link) == False:
-            time.sleep(10)
-            print '.'
-
-        postInfo['link'] = baseUrl + postInfo['permalink']
-        postInfo['shortlink'] = postInfo['link']
-        comment = GetCommentFromCommit(ref)
-        postInfo['subtitle'] = comment.encode('utf8')
-        try:
-            shortener = Shortener('Google', api_key= 'AIzaSyCuDCcM1utV1zbkiRDd-TX_8FrYT9ApISw')
-            postInfo['shortlink'] = shortener.short(postInfo['shortlink'])
-        except Exception as e:
-            print "Exception in shortener, using full url ", postInfo['shortlink']
-
-        imgUrl = urllib2.urlopen(img)
+        print('opening image to share')
+        imgUrl = urlopen(img)
         img = imgUrl.read()
-        st, st2 = MakeSharePost(postInfo)
-        print '========== cut here =========='
-        print st
-        print st2
-        print '========== cut here =========='
-        print '*** Publishing to Twitter'
-        PublishToTwitter(postInfo, img)
-        #print '*** Publishing to Facebook'
-        #PublishToFacebook(postInfo, st, img)
-        print '*** Done!'
-        #SearchAdoroCinema(postInfo)
-        webbrowser.open_new_tab(link)
-        if postInfo.has_key('imdb'):
+
+        print('publishing to twitter')
+        #PublishToTwitter(postInfo, img)
+        webbrowser.open_new_tab(postInfo['link'])
+        if 'imdb' in postInfo:
+            print('share on letterboxd, please')
             webbrowser.open_new_tab('http://www.letterboxd.com/imdb/' + postInfo['imdb'])
-        #webbrowser.open_new_tab('https://plus.google.com/b/116314610297829036822')
     except Exception as e:
-        print '*** Something gone wrong!'
+        print('something gone wrong')
         raise
 
 
 if len(sys.argv) < 3:
-    print 'How to use: share.py commit_or_head http://image-link'
+    print('How to use: share.py commit_or_head http://image-link')
 else:
     PublishToSocialMedia(sys.argv[1], sys.argv[2])
 
