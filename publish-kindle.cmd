eac2f3b7c216cf5d093afb2211ed70937269e19f
Wanderley Caloni <wanderley.caloni@bitforge.com.br>
2019-05-11 17:16:59 -0300

    Adicionando drafts do CinemAqui.

--- publish-kindle.cmd
+++ publish-kindle.cmd
@@ -1,5 +1,5 @@
 rem @echo off
-hugo --theme book --destination book
+hugo -D --theme book --destination book
 pushd book
 iconv -f UTF-8 -t LATIN1 index.html > book.html
 kindlegen.exe book.opf -o cinetenisverde.mobi
