a6cba05ededb51d2e573f9c8a78a663160956139
Wanderley Caloni <wanderley.caloni@bitforge.com.br>
2019-06-10 23:23:34 -0300

    Corrigido o nome.

--- publish-kindle.cmd
+++ publish-kindle.cmd
@@ -2,6 +2,6 @@ rem @echo off
 hugo -D --theme book --destination book
 pushd book
 iconv -f UTF-8 -t LATIN1 index.html > book.html
-kindlegen.exe book.opf -o cinetenisverde.mobi
-if exist k:\ copy /y cinetenisverde.mobi k:\documents
+kindlegen.exe book.opf -o caloni.mobi
+if exist k:\ copy /y caloni.mobi k:\documents
 popd
