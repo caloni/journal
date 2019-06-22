89cff8aca95f6fe99c43e0a30fe893022d0e2195
Wanderley Caloni <wanderley.caloni@bitforge.com.br>
2019-06-22 13:03:34 -0300

    Adicionando captura da imagem do pôster do imdb.

--- movies.py
+++ movies.py
@@ -5,6 +5,7 @@ def print_desc(imdb):
     ia = IMDb()
     movie = ia.get_movie(imdb)
     print("desc: \"", movie["title"], ". ", ", ".join(map(str, movie["countries"])), ", ", movie["year"], ". Dirigido por ", ", ".join(map(str, movie["director"])), ", escrito por ", ", ".join(map(str, movie["writer"])), ". Com ", ", ".join(map(str, list(movie["cast"])[0:3])), ".\"", sep="")
+    print("img: \"", movie["cover url"], "\"", sep="")
 
 if len(sys.argv) < 2:
     print('How to use: python imdb.py imdb')
