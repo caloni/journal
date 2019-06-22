0bc5d8943033ed88cb14f963ea6ea235810b50cf
Wanderley Caloni <wanderley.caloni@bitforge.com.br>
2019-06-22 12:37:17 -0300

    Script para obter info do imdb.

---
+++ movies.py
@@ -0,0 +1,13 @@
+import sys
+
+def print_desc(imdb):
+    from imdb import IMDb
+    ia = IMDb()
+    movie = ia.get_movie(imdb)
+    print("desc: \"", movie["title"], ". ", ", ".join(map(str, movie["countries"])), ", ", movie["year"], ". Dirigido por ", ", ".join(map(str, movie["director"])), ", escrito por ", ", ".join(map(str, movie["writer"])), ". Com ", ", ".join(map(str, list(movie["cast"])[0:3])), ".\"", sep="")
+
+if len(sys.argv) < 2:
+    print('How to use: python imdb.py imdb')
+else:
+    print_desc(sys.argv[1])
+
