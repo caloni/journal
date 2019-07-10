a73fdb5bc13bf2697808dfb191bf8b78d1f359a4
Wanderley Caloni <wanderley.caloni@bitforge.com.br>
2019-07-09 23:50:23 -0300

    Evitando crash quando não há cast.

--- movies.py
+++ movies.py
@@ -4,7 +4,7 @@ def print_desc(imdb):
     from imdb import IMDb
     ia = IMDb()
     movie = ia.get_movie(imdb)
-    print("desc: \"", movie["title"], ". ", ", ".join(map(str, movie["countries"])), ", ", movie["year"], ". Dirigido por ", ", ".join(map(str, movie["director"])), ", escrito por ", ", ".join(map(str, movie["writer"])), ". Com ", ", ".join(map(str, list(movie["cast"])[0:3])), ".\"", sep="")
+    print("desc: \"", movie["title"], ". ", ", ".join(map(str, movie["countries"])), ", ", movie["year"], ". Dirigido por ", ", ".join(map(str, movie["director"])), ", escrito por ", ", ".join(map(str, movie["writer"])), ". Com ", ", ".join(map(str, list(movie["cast"] if "cast" in movie else [])[0:3])), ".\"", sep="")
     print("img: \"", movie["cover url"], "\"", sep="")
 
 if len(sys.argv) < 2:
