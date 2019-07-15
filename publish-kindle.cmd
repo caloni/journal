db3d0f0fcd1b495a6d88b0effd4407f584a37934
Wanderley Caloni <wanderley.caloni@bitforge.com.br>
2019-07-15 20:19:31 -0300

    Voltando cópia do ebook.

--- publish-kindle.cmd
+++ publish-kindle.cmd
@@ -4,5 +4,5 @@ pushd book
 rem iconv -f UTF-8 -t LATIN1 index.html > book.html
 cp index.html book.html
 kindlegen.exe book.opf -o caloni.mobi
-rem if exist k:\ copy /y caloni.mobi k:\documents
+if exist k:\ copy /y caloni.mobi k:\documents
 popd
