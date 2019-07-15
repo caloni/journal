be8ca79195c0841450983c291cd036fb22d37010
Wanderley Caloni <wanderley.caloni@bitforge.com.br>
2019-07-15 15:04:55 -0300

    Retirando imagem dos posts (só vou compartilhar o que vale a pena, mesmo).

--- movies.py
+++ movies.py
@@ -5,7 +5,6 @@ def print_desc(imdb):
     ia = IMDb()
     movie = ia.get_movie(imdb)
     print("desc: \"", movie["title"], ". ", ", ".join(map(str, movie["countries"])), ", ", movie["year"], ". Dirigido por ", ", ".join(map(str, movie["director"])), ", escrito por ", ", ".join(map(str, movie["writer"])), ". Com ", ", ".join(map(str, list(movie["cast"] if "cast" in movie else [])[0:3])), ".\"", sep="")
-    print("img: \"", movie["cover url"], "\"", sep="")
 
 if len(sys.argv) < 2:
     print('How to use: python imdb.py imdb')
