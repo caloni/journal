2f528ac489c3522116f81856f11f44788abeb464
Wanderley Caloni <wanderley.caloni@bitforge.com.br>
2019-07-16 16:35:12 -0300

    Changing the new.cmd script, since there is no category anymore.

--- new.cmd
+++ new.cmd
@@ -1,10 +1,8 @@
 @echo off
-if "%2"=="" goto usage
-hugo new %1/%2.md
-gvim content\%1\%2.md
+if "%1"=="" goto usage
+hugo new post/%1.md
+gvim content\post\%1.md
 goto :eof
 
 :usage
-echo How to use: new [category] [slug]
-echo Categories: (without the .md)
-dir /b archetypes
+echo How to use: new [slug]
