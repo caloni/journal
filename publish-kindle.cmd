b66c01b4e12adf33de91828502b83c05240737e5
Wanderley Caloni <wanderley.caloni@bitforge.com.br>
2020-02-09 12:04:22 -0300

    Uses simple book.html as base to kindle publishing and discards book theme.

--- publish-kindle.cmd
+++ publish-kindle.cmd
@@ -1,8 +1,9 @@
 rem @echo off
-hugo -D --theme book --destination book
-pushd book
-rem iconv -f UTF-8 -t LATIN1 index.html > book.html
-cp index.html book.html
+copy /y layouts\book.html layouts\index.html
+hugo
+pushd public
+move /y index.html book.html
 kindlegen.exe book.opf -o caloni.mobi
 if exist k:\ copy /y caloni.mobi k:\documents
 popd
+git checkout layouts\index.html
