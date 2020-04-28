e969929a78ac34342ada0aa3b9cea80e5b600f90
Wanderley Caloni <wanderley@caloni.com.br>
2020-04-28 20:27:52 -0300

    Adiciona calonibot como submodule.

--- publish.cmd
+++ publish.cmd
@@ -5,12 +5,22 @@ git commit -m "Publishing."
 git pull
 git push
 popd
-git add --all
-git commit -m "Publishing."
+
 hugo
+
 pushd public
 git add --all
 git commit -m "Publishing."
 git push
 popd
+
+pushd calonibot
+git add --all
+git commit -m "Publishing."
+git pull
+git push
+popd
+
+git add --all
+git commit -m "Publishing."
 git push
