1934682532ad480ab079cc0af2fc295dfc0e8d2d
Wanderley Caloni <wanderleycaloni@gmail.com>
2020-05-17 18:06:52 -0300

    Atualiza bootstrap.

--- bootstrap.bat
+++ bootstrap.bat
@@ -8,9 +8,6 @@ git clone git@github.com:Caloni/caloni.github.io.git public
 pushd public
 git config core.autocrlf false
 popd
-pushd static
-git clone git@github.com:Caloni/images.git
-popd
 echo trying to install python requirements
 pip install -r requirements.txt
 echo all good; type publish to test and good luck!
