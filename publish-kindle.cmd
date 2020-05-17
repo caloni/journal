b348b1de8355147c94c20b5dc61a9b68a41ae01d
Wanderley Caloni <wanderleycaloni@gmail.com>
2020-05-16 21:07:46 -0300

    Retirando pasta de imagens.

--- publish-kindle.cmd
+++ publish-kindle.cmd
@@ -3,9 +3,9 @@ copy /y layouts\book.html layouts\index.html
 hugo
 pushd public
 move /y index.html book.html
-sed -i "s/href=\"\//href=\"#/g" book.html
-sed -i "s/src=\"\//src=\"/g" book.html
-sed -i "s/href=\"#images/href=\"images/g" book.html
+rem sed -i "s/href=\"\//href=\"#/g" book.html
+rem sed -i "s/src=\"\//src=\"/g" book.html
+rem sed -i "s/href=\"#images/href=\"images/g" book.html
 kindlegen.exe book.opf -o caloni.mobi
 if exist k:\ copy /y caloni.mobi k:\documents
 popd
