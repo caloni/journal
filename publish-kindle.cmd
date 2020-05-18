faf274844e96a5c0e997e3e207ff9789e30de3cd
Wanderley Caloni <wanderleycaloni@gmail.com>
2020-05-17 21:44:59 -0300

    Publishing.

--- publish-kindle.cmd
+++ publish-kindle.cmd
@@ -3,9 +3,6 @@ copy /y layouts\book.html layouts\index.html
 hugo
 pushd public
 move /y index.html book.html
-rem sed -i "s/href=\"\//href=\"#/g" book.html
-rem sed -i "s/src=\"\//src=\"/g" book.html
-rem sed -i "s/href=\"#images/href=\"images/g" book.html
 kindlegen.exe book.opf -o caloni.mobi
 if exist k:\ copy /y caloni.mobi k:\documents
 popd
