320bb32cd79a5a1309303aabf4c43c9657a6ef16
Wanderley Caloni <wanderleycaloni@gmail.com>
2020-05-30 18:08:29 -0300

    publish: Melhora formatação de ascii art e coloca marcador para publicar livro.

--- publish-kindle.cmd
+++ publish-kindle.cmd
@@ -3,6 +3,7 @@ copy /y layouts\book.html layouts\index.html
 hugo --buildDrafts
 pushd public
 move /y index.html book.html
+sed -i "s/<pre><code>ascii art:/<h6 style=\"page-break-after: always\"><\/h6><pre><code>/" book.html
 kindlegen.exe book.opf -o caloni.mobi
 if exist k:\ copy /y caloni.mobi k:\documents
 popd
