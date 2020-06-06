7c67d962e48217499028fa0c58c8fc32fe7e6698
Wanderley Caloni <wanderleycaloni@gmail.com>
2020-06-05 22:20:58 -0300

    Adiciona tags atuais para facilitar categorização dos novos posts.

--- new.cmd
+++ new.cmd
@@ -1,6 +1,8 @@
 @echo off
 if "%1"=="" goto usage
 hugo new post/%1.md
+cat public\tags\index.xml | grep "guid" | sed "s/.*tags\/\([a-z]\+\).*/\1/" | "\Program Files\Git\usr\bin\sort.exe" | paste -s -d" " - | sed -e "s/ /\", \"/g" -e "s/^/[ \"/" -e "s/$/\" ]/" >> content\post\%1.md
+unix2dos content\post\%1.md
 gvim content\post\%1.md
 goto :eof
 
