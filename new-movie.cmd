48ffdeba107cd7fd65e97d4651c016ca47aff90f
Wanderley Caloni <wanderleycaloni@gmail.com>
2020-06-19 02:19:45 -0300

    Revert "Não haverá mais buscar por filmes."
    This reverts commit 15e458dc33e0c4708c6fc0a51e77e386bad51a9d.

---
+++ new-movie.cmd
@@ -0,0 +1,9 @@
+@echo off
+if "%1"=="" goto usage
+hugo new post/%1.md
+python static\code\movies.py search %1 data\post\%1.toml
+gvim content\post\%1.md data\post\%1.toml
+goto :eof
+
+:usage
+echo How to use: new [slug] [imdb]
