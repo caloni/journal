c6f649d3a832c9b380a11ee240c2e9c96bd1eb98
Wanderley Caloni <wanderleycaloni@gmail.com>
2020-06-19 02:20:11 -0300

    Revert "Sem bootstrap."
    This reverts commit b60d4509006cb25f1f2252738324bf8c2874b64b.

---
+++ bootstrap.bat
@@ -0,0 +1,13 @@
+@echo off
+git submodule init
+git submodule update
+pushd content\post
+git checkout master
+popd
+git clone git@github.com:Caloni/caloni.github.io.git public
+pushd public
+git config core.autocrlf false
+popd
+echo trying to install python requirements
+pip install -r requirements.txt
+echo all good; type publish to test and good luck!
