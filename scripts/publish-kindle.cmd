93e3efd00e0c888ef6df31fa86ebaecf58434918
Wanderley Caloni <wanderley.caloni@intelitrader.com.br>
2020-08-20 22:36:56 -0300

    publish:

--- scripts/publish-kindle.cmd
+++ scripts/publish-kindle.cmd
@@ -1,10 +1,10 @@
 rem @echo off
 copy /y layouts\book.html layouts\index.html
 hugo --buildDrafts
-pushd public
-move /y index.html book.html
-sed -i "s/<pre><code>ascii art:/<h6 style=\"page-break-after: always\"><\/h6><pre><code>/" book.html
-kindlegen.exe book.opf -o caloni.mobi
-if exist k:\ mv caloni.mobi k:\documents
-popd
 git checkout layouts\index.html
+pushd public\book
+move /y ..\index.html .
+sed -i "s/<pre><code>ascii art:/<h6 style=\"page-break-after: always\"><\/h6><pre><code>/" index.html
+kindlegen.exe book.opf -o caloni.mobi
+if exist k:\ cp caloni.mobi k:\documents
+popd
