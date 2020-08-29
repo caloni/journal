f6963cfd93b8bbc82e9fa98180451dcea00f2a42
Wanderley Caloni <wanderley.caloni@intelitrader.com.br>
2020-08-29 19:02:10 -0300

    Retira uso de sed no script kindle.

--- scripts/publish-kindle.cmd
+++ scripts/publish-kindle.cmd
@@ -4,7 +4,6 @@ hugo --buildDrafts
 git checkout layouts\index.html
 pushd public\book
 move /y ..\index.html .
-sed -i "s/<pre><code>ascii art:/<h6 style=\"page-break-after: always\"><\/h6><pre><code>/" index.html
 kindlegen.exe book.opf -o caloni.mobi
 if exist k:\ cp caloni.mobi k:\documents
 popd
