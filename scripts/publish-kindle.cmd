195223fb1d6ef11277ec91e97364ba1a674d9b05
Wanderley Caloni <wanderley.caloni@intelitrader.com.br>
2020-09-12 15:18:43 -0300

    book: agora que voltei a referenciar os posts, corrigindo referências.

--- scripts/publish-kindle.cmd
+++ scripts/publish-kindle.cmd
@@ -4,6 +4,8 @@ hugo --buildDrafts
 git checkout layouts\index.html
 pushd public\book
 move /y ..\index.html .
+sed -i "s/href=\"\//href=\"#/g" index.html
+sed -i "s/href=\"#[a-z]\+\/[a-z.-]\+/href=\"#/g" index.html
 kindlegen.exe book.opf -o caloni.mobi
 if exist k:\ cp caloni.mobi k:\documents
 popd
