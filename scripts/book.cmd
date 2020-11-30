63249766a941facb595e81e207638cecbb92e85d
Wanderley Caloni <wanderley.caloni@intelitrader.com.br>
2020-11-29 21:12:22 -0300

    blog: divide themes in blog and book.

--- scripts/book.cmd
+++ scripts/book.cmd
@@ -1,9 +1,6 @@
 rem @echo off
-copy /y layouts\book.html layouts\index.html
-hugo --buildDrafts
-git checkout layouts\index.html
-pushd public\book
-move /y ..\index.html .
+hugo --buildDrafts --theme=book -d book
+pushd book
 sed -i "s/href=\"\//href=\"#/g" index.html
 sed -i "s/href=\"#[a-z]\+\/[a-z.-]\+/href=\"#/g" index.html
 kindlegen.exe book.opf -o caloni.mobi
