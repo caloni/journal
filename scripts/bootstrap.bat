1446e9e8824c0621a4bf91ab718598b6b726dbd9
Wanderley Caloni <wanderley.caloni@intelitrader.com.br>
2020-11-29 21:16:27 -0300

    update bootstrap script.

--- scripts/bootstrap.bat
+++ scripts/bootstrap.bat
@@ -1,13 +1,6 @@
 @echo off
-git submodule init
-git submodule update
-pushd content\post
-git checkout master
-popd
 git clone git@github.com:Caloni/caloni.github.io.git public
 pushd public
 git config core.autocrlf false
 popd
-echo trying to install python requirements
-pip install -r requirements.txt
-echo all good; type publish to test and good luck!
+echo all good; type hugo to test and good luck!
