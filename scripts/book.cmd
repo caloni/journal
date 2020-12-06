44fe409eeb53204783fcc8e53b05d69327dbe7e1
Wanderley Caloni <wanderley.caloni@intelitrader.com.br>
2020-12-06 19:47:44 -0300

    remove auto-copy.

--- scripts/book.cmd
+++ scripts/book.cmd
@@ -4,5 +4,4 @@ pushd book
 sed -i "s/href=\"\//href=\"#/g" index.html
 sed -i "s/href=\"#[a-z]\+\/[a-z.-]\+/href=\"#/g" index.html
 kindlegen.exe book.opf -o caloni.mobi
-if exist k:\ cp caloni.mobi k:\documents
 popd
