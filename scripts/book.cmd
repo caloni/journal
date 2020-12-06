f955f0da8ff37b32632206d4c6ef5a63faf7ef8a
Wanderley Caloni <wanderley.caloni@intelitrader.com.br>
2020-12-06 20:39:49 -0300

    Revert "remove auto-copy."
    This reverts commit 44fe409eeb53204783fcc8e53b05d69327dbe7e1.

--- scripts/book.cmd
+++ scripts/book.cmd
@@ -4,4 +4,5 @@ pushd book
 sed -i "s/href=\"\//href=\"#/g" index.html
 sed -i "s/href=\"#[a-z]\+\/[a-z.-]\+/href=\"#/g" index.html
 kindlegen.exe book.opf -o caloni.mobi
+if exist k:\ cp caloni.mobi k:\documents
 popd
