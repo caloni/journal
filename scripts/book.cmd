4919ec91e75fa6a77641c4182163eb3eadccc324
Wanderley Caloni <wanderleycaloni@gmail.com>
2021-01-10 14:14:26 -0300

    fix images local path

--- scripts/book.cmd
+++ scripts/book.cmd
@@ -2,6 +2,7 @@ rem @echo off
 hugo --buildDrafts --theme=book -d book
 pushd book
 sed -i "s/href=\"\//href=\"#/g" index.html
+sed -i "s/src=\"\//src=\"/g" index.html
 sed -i "s/href=\"#[a-z]\+\/[a-z.-]\+/href=\"#/g" index.html
 kindlegen.exe book.opf -o caloni.mobi
 if exist k:\ cp caloni.mobi k:\documents
