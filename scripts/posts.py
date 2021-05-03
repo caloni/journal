74215ab062bf97d5685d3ca8810af69aa9d5f573
Wanderley Caloni <wanderleycaloni@gmail.com>
2021-05-02 23:05:46 -0300

    new useful functions to script.

--- scripts/posts.py
+++ scripts/posts.py
@@ -1,11 +1,14 @@
 import frontmatter
-import glob
+from glob import glob
+
 
 path = "content/posts/*.md"
+files = [ glob(path) ]
 verbose = False
 
+
 def transform(operations):
-  for fname in glob.glob(path):
+  for fname in files:
     if verbose:
       print(fname)
     with open(fname, 'r', encoding='utf8') as f:
@@ -17,20 +20,65 @@ def transform(operations):
       with open(fname, 'w', encoding='utf8') as f:
         f.write(post_str)
 
+
 def format():
   def nothing(post):
     pass
   transform([ nothing ])
 
+
+def add_item(post, collection, item):
+  if post.get(collection):
+    if item not in post[collection]:
+      post[collection].append(item)
+      return True
+  else:
+    post[collection] = [ item ]
+    return True
+  return False
+
+
+def remove_item(post, collection, item):
+  if post.get(collection) and item in post[collection]:
+    post[collection].remove(item)
+    return True
+  return False
+
+
 def move_category_to_tag(category):
   def func(post):
-    if post.get('categories') and category in post['categories']:
-      if verbose:
-        print('categories:', post['categories'])
-      post['categories'].remove(category)
-      if post.get('tags') and category not in post['tags']:
-        post['tags'].append(category)
-      else:
-        post['tags'] = [ category ]
+    if remove_item(post, 'categories', category):
+      add_item(post, 'tags', category)
+  transform([ func ])
+
+
+def move_tag_to_category(tag):
+  def func(post):
+    if remove_item(post, 'tags', tag):
+      add_item(post, 'categories', tag)
+  transform([ func ])
+
+
+def add_tag(tag):
+  def func(post):
+    add_item(post, 'tags', tag)
+  transform([ func ])
+
+
+def add_category(category):
+  def func(post):
+    add_item(post, 'categories', category)
+  transform([ func ])
+
+
+def remove_tag(tag):
+  def func(post):
+    remove_item(post, 'tags', tag)
+  transform([ func ])
+
+
+def remove_category(category):
+  def func(post):
+    remove_item(post, 'categories', category)
   transform([ func ])
 
