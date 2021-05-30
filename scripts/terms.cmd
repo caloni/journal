cda572acdd473f5c4dfad5c318268b1307d3b057
Wanderley Caloni <wanderleycaloni@gmail.com>
2021-05-30 14:03:40 -0300

    best terms script output.

--- scripts/terms.cmd
+++ scripts/terms.cmd
@@ -1,7 +1,5 @@
 @echo off
-echo categories
-echo ==========
+echo == categories ==
 grep "guid" public\categories\index.xml | sed "s/.*categories\/\([a-z]\+\).*/\1/" | sort
-echo tags
-echo ====
+echo == tags ==
 grep "guid" public\tags\index.xml | sed "s/.*tags\/\([a-z]\+\).*/\1/" | sort
