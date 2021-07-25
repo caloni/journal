76faf6a2f225bfcd5a513b3bea2c9618c4c2650a
Wanderley Caloni <wanderleycaloni@gmail.com>
2021-07-24 22:12:14 -0300

    add script to create new post from clipboard.

---
+++ scripts/new.py
@@ -0,0 +1,18 @@
+import posts
+import subprocess
+import sys
+import pyperclip
+
+if len(sys.argv) > 1:
+  file = "posts/{}.md".format(sys.argv[1])
+  proc = subprocess.Popen("hugo new {}".format(file), stdout=subprocess.PIPE, shell=True)
+  proc.wait()
+  if proc.returncode == 0:
+    clip = pyperclip.paste().splitlines()
+    def edit(post):
+      post['title'] = clip[0]
+      post.content = '\n'.join(clip[2:])
+    posts.transform([ edit ], [ 'content/' + file ])
+else:
+  print('How to use: new [slug]')
+
