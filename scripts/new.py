5dfc9e02cb7506c869ade3f7ba4a242c034eb231
Wanderley Caloni <wanderleycaloni@gmail.com>
2021-08-30 22:57:48 -0300

    existing file is not a error anymore.

--- scripts/new.py
+++ scripts/new.py
@@ -7,12 +7,11 @@ if len(sys.argv) > 1:
   file = "posts/{}.md".format(sys.argv[1])
   proc = subprocess.Popen("hugo new {}".format(file), stdout=subprocess.PIPE, shell=True)
   proc.wait()
-  if proc.returncode == 0:
-    clip = pyperclip.paste().splitlines()
-    def edit(post):
-      post['title'] = clip[0]
-      post.content = '\n'.join(clip[2:])
-    posts.transform([ edit ], [ 'content/' + file ])
+  clip = pyperclip.paste().splitlines()
+  def edit(post):
+    post['title'] = clip[0]
+    post.content = '\n'.join(clip[2:])
+  posts.transform([ edit ], [ 'content/' + file ])
 else:
   print('How to use: new [slug]')
 
