cd8ea3ac827f727ab4928fbef5d7a1518f66829b
Wanderley Caloni <wanderleycaloni@gmail.com>
2021-12-10 23:28:40 -0300

    new theme drafts.

--- scripts/book.cmd
+++ scripts/book.cmd
@@ -1,4 +1,16 @@
-rem @echo off
+@echo off
+echo Building drafts...
+hugo --buildDrafts --theme=drafts -d drafts
+pushd drafts
+sed -i "s/href=\"\//href=\"#/g" index.html
+sed -i "s/src=\"\//src=\"/g" index.html
+sed -i "s/href=\"#[a-z]\+\/[a-z.-]\+/href=\"#/g" index.html
+kindlegen.exe book.opf -o caloni-drafts.mobi
+if exist k:\ cp caloni-drafts.mobi k:\documents
+popd
+
+choice /M "Do you want to build the entire book (default yes in 10 seconds)?" /D Y /T 10
+if errorlevel 2 goto :eof
 hugo --buildDrafts --theme=book -d book
 pushd book
 sed -i "s/href=\"\//href=\"#/g" index.html
@@ -7,3 +19,4 @@ sed -i "s/href=\"#[a-z]\+\/[a-z.-]\+/href=\"#/g" index.html
 kindlegen.exe book.opf -o caloni.mobi
 if exist k:\ cp caloni.mobi k:\documents
 popd
+
