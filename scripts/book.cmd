4522a627b3f91011fc502ff06daf9109660858bd
Wanderley Caloni <wanderleycaloni@gmail.com>
2021-12-12 20:49:37 -0300

    zero drafts marker in kindle.

--- scripts/book.cmd
+++ scripts/book.cmd
@@ -7,6 +7,7 @@ sed -i "s/src=\"\//src=\"/g" index.html
 sed -i "s/href=\"#[a-z]\+\/[a-z.-]\+/href=\"#/g" index.html
 kindlegen.exe book.opf -o caloni-drafts.mobi
 if exist k:\ cp caloni-drafts.mobi k:\documents
+if exist k:\ rm k:\documents\caloni-drafts.han
 popd
 
 choice /M "Do you want to build the entire book (default yes in 10 seconds)?" /D Y /T 10
