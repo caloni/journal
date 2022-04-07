4667ed82b7d068ec88bbbce295ee343b7ca63076
Wanderley Caloni <wanderleycaloni@gmail.com>
2022-04-07 12:34:11 -0300

    use default publish msg.

--- scripts/blog.cmd
+++ scripts/blog.cmd
@@ -1,6 +1,6 @@
 @echo off
 git add --all
-git commit -m "publish: %*"
+git commit -m "new posts, updates and promotions. %*"
 git pull
 git push
 
@@ -9,6 +9,6 @@ hugo
 pushd public
 git add -f tags
 git add --all
-git commit -m "publish: %*"
+git commit -m "new posts, updates and promotions. %*"
 git push
 popd
