6d7077ac70b7bbe0fc9a682f2722502c4f34004d
Wanderley Caloni <wanderleycaloni@gmail.com>
2022-04-10 21:48:24 -0300

    separate compile/commit from publish

--- scripts/blog.cmd
+++ scripts/blog.cmd
@@ -1,23 +1,14 @@
 @echo off
-hugo
-if %ERRORLEVEL% neq 0 goto :eof
-
 git add --all
 git commit -m "new posts, updates and promotions. %*"
+git pull
+git push
+
+hugo
 
 pushd public
 git add -f tags
 git add --all
 git commit -m "new posts, updates and promotions. %*"
-popd
-
-choice /M "Do you want to publish (default yes in 10 seconds)?" /D Y /T 10
-if errorlevel 2 goto :eof
-
-git pull
-git push
-
-pushd public
 git push
 popd
-
