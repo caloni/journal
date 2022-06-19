cc839569ad444bfd9d0ef4cc99049ac88b94641c
Wanderley Caloni <wanderleycaloni@gmail.com>
2022-06-19 20:03:09 -0300

    script to record new games easily.

---
+++ scripts/chess.py
@@ -0,0 +1,34 @@
+import posts
+import subprocess
+import sys
+import pyperclip
+import os
+import re
+
+if len(sys.argv) > 1:
+  baseName = sys.argv[1]
+  file = "posts/{}.md".format(baseName)
+  link = None
+
+  proc = subprocess.Popen("hugo new chess/{}".format(file), stdout=subprocess.PIPE, shell=True)
+  proc.wait()
+  os.rename("content/chess/posts/{}.md".format(baseName), "content/posts/{}.md".format(baseName))
+
+  with open("static/chess/{}.txt".format(baseName), "w", encoding="utf8") as f:
+    def findLink(clip):
+      for c in clip:
+        srch = re.search('^\[Link "(.*)"\]', c)
+        if srch:
+          return srch.groups(1)[0]
+    clip = pyperclip.paste().splitlines()
+    f.writelines('\n'.join(clip[0:]))
+    link = findLink(clip)
+
+  if link:
+    def edit(post, fname):
+      post['link'] = link
+    posts.transform([ edit ], [ 'content/' + file ])
+
+else:
+  print('How to use: chess.py [slug]')
+
