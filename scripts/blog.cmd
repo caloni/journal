177e928b0440f0de8c551916694a879d99dd22f4
Wanderley Caloni <wanderleycaloni@gmail.com>
2022-12-19 13:08:57 -0300

    do not commit changes before publishing.

--- scripts/blog.cmd
+++ scripts/blog.cmd
@@ -1,14 +1,9 @@
 @echo off
-git add --all
-git commit -m "new posts."
 git pull
-git push
-
 hugo -D --cleanDestinationDir
 
 pushd public
-git add -f tags
 git add --all
-git commit -m "new posts."
-git push
+git commit -m "publishing."
+git push -f
 popd
