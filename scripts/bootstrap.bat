f08ff9bba398ea0dcd58df98c93858b0677c0016
Wanderley Caloni <wanderleycaloni@gmail.com>
2023-01-28 19:10:03 -0300

    update bootstrap to new dirs and schema (fix tags ignored).

--- scripts/bootstrap.bat
+++ scripts/bootstrap.bat
@@ -1,6 +1,10 @@
 @echo off
-git clone git@github.com:Caloni/caloni.github.io.git public
-pushd public
-git config core.autocrlf false
+if not exist public mkdir public
+if exist public\blog.bkp rm -fr public\blog.bkp
+if exist public\blog mv public\blog public\blog.bkp
+git clone git@github.com:Caloni/caloni.github.io.git public\blog
+pushd public\blog
+git config --local core.autocrlf false
+git config --local core.excludesfile false
 popd
-echo all good; type hugo to test and good luck!
+echo all good; type scripts\blog to test and good luck!
