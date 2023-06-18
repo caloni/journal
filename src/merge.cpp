77f301704589f2f230acbb8c2a6ed56f3653f1d7
Wanderley Caloni <wanderley.caloni@gmail.com>
2023-06-17 21:53:50 -0300

    A silly try to use awk as parser.

--- src/merge.cpp
+++ src/merge.cpp
@@ -3,9 +3,20 @@
 #include <fstream>
 #include <sstream>
 #include <filesystem>
+#include <awklib/awk.h>
+
 
 using namespace std;
 namespace fs = std::filesystem;
+
+struct PostHeader
+{
+    string date;
+    string title;
+    vector<string> categories;
+    vector<string> tags;
+    string link;
+};
 
 
 std::string ReadEntireFile(std::ifstream& in)
@@ -13,6 +24,47 @@ std::string ReadEntireFile(std::ifstream& in)
     std::ostringstream sstr;
     sstr << in.rdbuf();
     return sstr.str();
+}
+
+
+istringstream g_is;
+static int myinproc()
+{
+    return g_is.get();
+}
+
+void setfield(AWKINTERP *pinter, awksymb* ret, int nargs, awksymb* args)
+{
+}
+
+PostHeader ReadYamlHeader(const string& content)
+{
+    PostHeader header;
+    size_t yamlBegin = content.find("---\n");
+    size_t yamlEnd = content.find("\n---\n", yamlBegin);
+    bool valid = yamlBegin != content.npos
+        && yamlEnd != content.npos;
+
+    if( valid )
+    {
+        size_t headerBegin = yamlBegin + 4;
+        size_t headerEnd = yamlEnd - headerBegin;
+        string headerStr = content.substr(yamlBegin + 4, headerEnd);
+
+        AWKINTERP* interp = awk_init(NULL);
+        awk_setprog(interp, "/^title:/ {setfield($0)}");
+        awk_compile(interp);
+        g_is = istringstream(headerStr);
+        awk_infunc(interp, myinproc);
+        awk_addfunc(interp, "setfield", setfield, 1);
+        awk_exec(interp);
+        awksymb symb{};
+        symb.name = "title";
+        int res = awk_getvar(interp, &symb);
+        awk_end(interp);
+    }
+
+    return header;
 }
 
 
@@ -34,6 +86,7 @@ int main()
                 if (ifs)
                 {
                     string content = ReadEntireFile(ifs);
+                    //PostHeader header = ReadYamlHeader(content);
                     ofs << "\n" << content << endl;
                 }
             }
