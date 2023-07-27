89925bc241ec9d2aca54f20dcbbf9e7bc7cae141
Wanderley Caloni <wanderley.caloni@gmail.com>
2023-07-26 23:25:46 -0300

    Clean output before copying.

--- scripts/awk_alpha.cmd
+++ scripts/awk_alpha.cmd
@@ -1,6 +1,7 @@
 @echo off
 scripts\merge.exe
 gawk -f scripts\md2adoc.awk content\blog.md > content\blog.txt
+rm -fr public\epub_awk
 xcopy /E /I /Y themes\epub_awk public\epub_awk
 echo Generating single files...
 gawk -f scripts\adoc2epub.awk content\blog.txt
