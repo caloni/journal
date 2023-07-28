32447a39cec9b1c168e26834b67a036d13cc0b8e
Wanderley Caloni <wanderley.caloni@gmail.com>
2023-07-27 23:22:41 -0300

    Fix encoding issues.

--- scripts/awk_alpha.cmd
+++ scripts/awk_alpha.cmd
@@ -4,4 +4,7 @@ gawk -f scripts\md2adoc.awk content\blog.md > content\blog.txt
 rm -fr public\epub_awk
 xcopy /E /I /Y themes\epub_awk public\epub_awk
 echo Generating single files...
+setlocal
+set LC_ALL=en_US.UTF-8
 gawk -f scripts\adoc2epub.awk content\blog.txt
+endlocal
