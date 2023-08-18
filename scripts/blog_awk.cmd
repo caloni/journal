7642ff98e0bce48d7e49058c84915e429eca7503
Wanderley Caloni <wanderley.caloni@gmail.com>
2023-08-18 20:46:11 -0300

    Organize clean and copy theme files.

--- scripts/blog_awk.cmd
+++ scripts/blog_awk.cmd
@@ -1,9 +1,9 @@
 @echo off
-scripts\merge.exe
-gawk -f scripts\removeyaml.awk content\blog.md > content\blog.txt
 rm -fr public\blog_awk
 xcopy /E /I /Y themes\blog\static public\blog_awk
-echo Generating single files...
+scripts\merge.exe
+gawk -f scripts\removeyaml.awk content\blog.md > content\blog.txt
+echo Single blog file converted from md+yaml to txt.
 setlocal
 set LC_ALL=en_US.UTF-8
 gawk -f scripts\txt2blog.awk content\blog.txt
