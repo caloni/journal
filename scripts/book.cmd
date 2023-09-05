1af5371a40dc7e7c9647f0a17138dcf4e5299b6b
Wanderley Caloni <wanderley.caloni@gmail.com>
2023-09-05 15:21:05 -0300

    Restore the journal copy and create a drafts one.
    The target here is to agilize drafting and avoid to construct kindle mobi
    everytime there is new text to be revised.

--- scripts/book.cmd
+++ scripts/book.cmd
@@ -10,11 +10,19 @@ pushd public\book
 call repack.cmd
 echo Book generated
 popd
-choice /M "Do you want to convert and copy to kindle (default yes in 10 seconds)?" /D Y /T 10
-if errorlevel 2 goto :eof
 pushd public\book
+choice /M "Do you want to convert and copy to kindle (default yes in 10 seconds)?" /D Y /T 10
+if errorlevel 2 goto :copy_notes
 call tokindle.cmd
 copy /y caloni.mobi k:\documents
 if %ERRORLEVEL% EQU 0 echo === BOOK COPIED SUCCESSFULLY ===
 if %ERRORLEVEL% NEQ 0 echo === ERROR COPYING BOOK ===
+:copy_notes
+rm k:\documents\drafts.txt
+copy /y drafts.txt k:\documents
+if %ERRORLEVEL% EQU 0 echo === DRAFTS UPDATED ===
+if %ERRORLEVEL% NEQ 0 echo === ERROR UPDATING DRAFTS ===
 popd
+copy /y ..\journal.txt k:\documents
+if %ERRORLEVEL% EQU 0 echo === JOURNAL UPDATED ===
+if %ERRORLEVEL% NEQ 0 echo === ERROR UPDATING JOURNAL ===
