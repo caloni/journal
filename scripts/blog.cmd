d03add6a969c8246dfdf6ce4e664a4cc8a0ec8fb
Wanderley Caloni <wanderley.caloni@gmail.com>
2023-11-16 14:46:36 -0300

    Publish the sources before the blog.

--- scripts/blog.cmd
+++ scripts/blog.cmd
@@ -9,11 +9,12 @@ set LC_ALL=en_US.UTF-8
 gawk -f scripts\txt2blog.awk blog.txt
 endlocal
 echo Blog generated
-choice /M "Do you want to publish the blog to the world wide web (default yes in 10 seconds)?" /D Y /T 10
-if errorlevel 2 goto :eof
 git add --all
 git commit -m "Publish new version."
 git push
+echo Source published
+choice /M "Do you want to publish the blog to the world wide web (default yes in 10 seconds)?" /D Y /T 10
+if errorlevel 2 goto :eof
 pushd public\blog
 git add --all
 git commit -m "Publish new version."
