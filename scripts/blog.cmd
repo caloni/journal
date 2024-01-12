f106a808a030716ea282c1861c41e79070542683
Wanderley Caloni <wanderley.caloni@gmail.com>
2024-01-12 17:06:11 -0300

    Change script messages.

--- scripts/blog.cmd
+++ scripts/blog.cmd
@@ -12,13 +12,13 @@ gawk -f scripts\txt2blog.awk blog.txt
 endlocal
 echo Blog generated
 git add --all
-git commit -m "Publish new version."
+git commit -m "Another day flawlessly finished."
 git push
 echo Source published
-choice /M "Do you want to publish the blog to the world wide web (default yes in 10 seconds)?" /D Y /T 10
+choice /M "Do you want to publish the PUBLIC blog to the world wide web (default yes in 10 seconds)?" /D Y /T 10
 if errorlevel 2 goto :eof
 pushd public\blog
 git add --all
-git commit -m "Publish new version."
+git commit -m "Here comes great news."
 git push
 popd
