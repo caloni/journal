bea995527a93cf0f4cb84d1245c46ec79dd319da
Wanderley Caloni <wanderley.caloni@gmail.com>
2024-02-17 21:30:15 -0300

    remove journal from ebook generator.
    We don't need the journal content into the reader. We just need what
    is published.

--- scripts/book.cmd
+++ scripts/book.cmd
@@ -9,8 +9,8 @@ xcopy /Q /E /I /Y img\journal public\book\MOBI\img
 xcopy /Q /E /I /Y img\blog public\book\MOBI\img
 setlocal
 set LC_ALL=en_US.UTF-8
-gawk -f scripts\txt2epub.awk blog.txt journal.txt
-gawk -f scripts\txt2mobi.awk blog.txt journal.txt
+gawk -f scripts\txt2epub.awk blog.txt
+gawk -f scripts\txt2mobi.awk blog.txt
 endlocal
 pushd public\book
 call repack.cmd
