8e82a9795abc2bc0c9bc1252ac8bbf2c7022790f
Wanderley Caloni <wanderley.caloni@gmail.com>
2024-03-10 15:34:12 -0300

    tired of trying to convert direct from mobi.

--- book/tokindle.cmd
+++ book/tokindle.cmd
@@ -1,5 +1,5 @@
-rem kindlegen.exe caloni.epub -o caloni.mobi
-pushd MOBI
-kindlegen.exe book.opf -o caloni.mobi
-if exist caloni.mobi copy /y caloni.mobi ..
-popd
+kindlegen.exe caloni.epub -o caloni.mobi
+rem pushd MOBI
+rem kindlegen.exe book.opf -o caloni.mobi
+rem if exist caloni.mobi copy /y caloni.mobi ..
+rem popd
