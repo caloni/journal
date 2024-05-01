71f9e66d598a8f28a58a96679bef54df85cec43e
Wanderley Caloni <wanderley.caloni@gmail.com>
2024-05-01 15:12:03 -0300

    now my blog.txt is the journal.

--- scripts/blog.cmd
+++ scripts/blog.cmd
@@ -8,7 +8,7 @@ xcopy /Q /E /I /Y blog public\blog
 xcopy /Q /E /I /Y img\blog public\blog\img
 setlocal
 set LC_ALL=en_US.UTF-8
-gawk -f scripts\txt2blog.awk blog.txt
+gawk -f scripts\txt2blog.awk journal.txt
 endlocal
 echo Blog generated
 git add --all
