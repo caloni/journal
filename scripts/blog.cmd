f625f0efc07c0eb63204cc67035e8b746c6d7bff
Wanderley Caloni <wanderley.caloni@gmail.com>
2024-08-02 14:06:33 -0300

    Publishing some shit.

--- scripts/blog.cmd
+++ scripts/blog.cmd
@@ -1,16 +1,5 @@
 @echo off
-if not exist public mkdir public
-if not exist public\blog mkdir public\blog
-pushd public\blog
-rm -fr *
-popd
-xcopy /Q /E /I /Y blog public\blog
-xcopy /Q /E /I /Y img\blog public\blog\img
-setlocal
-set LC_ALL=en_US.UTF-8
-gawk -f scripts\txt2blog.awk journal.txt
-endlocal
-echo Blog generated
+call blogbuild.cmd
 git add --all
 git commit -m "Publishing some shit."
 git push
