39411a21e05be4ab70aa1cda0c98960c0fbcd8ea
Wanderley Caloni <wanderley.caloni@gmail.com>
2024-11-12 19:27:03 -0300

    publishing some shit.

--- scripts/blog.py
+++ scripts/blog.py
@@ -7,17 +7,17 @@ if process.stdout:
 if process.returncode:
   print('blogbuild.py returned', process.returncode)
 
-process = subprocess.run(['git', 'add', '--all'], check=True)
-process = subprocess.run(['git', 'commit', '-m', 'publishing some shit.'], check=True)
-process = subprocess.run(['git', 'push'], check=True)
+process = subprocess.run(['git', 'add', '--all'], check=False)
+process = subprocess.run(['git', 'commit', '-m', 'publishing some shit.'], check=False)
+process = subprocess.run(['git', 'push'], check=False)
 
 print('do you want to publish the PUBLIC blog to the world wide web? (Y/N)')
 publish_answer = input()
 if 'yYsS1yessim'.find(publish_answer) != -1:
   old_dir = os.getcwd()
   os.chdir(r'public/blog')
-  process = subprocess.run(['git', 'add', '--all'], check=True)
-  process = subprocess.run(['git', 'commit', '-m', 'here comes great news.'], check=True)
-  process = subprocess.run(['git', 'push'], check=True)
+  process = subprocess.run(['git', 'add', '--all'], check=False)
+  process = subprocess.run(['git', 'commit', '-m', 'here comes great news.'], check=False)
+  process = subprocess.run(['git', 'push'], check=False)
   os.chdir(old_dir)
 
