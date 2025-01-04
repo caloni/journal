edaf1bc7c0606d172201890f0c820cd24fe2b963
Wanderley Caloni <wanderley.caloni@gmail.com>
2025-01-04 10:37:39 -0300

    remove asking to publish from the publish script.

--- code/PublishBlog.py
+++ code/PublishBlog.py
@@ -7,9 +7,6 @@ os.chdir(dname)
 os.chdir('..')
 print('basedir:', os.getcwd())
 
-print('do you want to publish the PUBLIC blog to the world wide web? (Y/N)')
-publish_answer = input()
-
 process = subprocess.run(['python', r'code/Journal2Blog.py'], check=True)
 if process.stdout:
   print(process.stdout)
@@ -20,11 +17,9 @@ process = subprocess.run(['git', 'add', '--all'], check=False)
 process = subprocess.run(['git', 'commit', '-m', 'publishing some shit.'], check=False)
 process = subprocess.run(['git', 'push'], check=False)
 
-if 'yYsS1yessim'.find(publish_answer) != -1:
-  old_dir = os.getcwd()
-  os.chdir(r'public/blog')
-  process = subprocess.run(['git', 'add', '--all'], check=False)
-  process = subprocess.run(['git', 'commit', '-m', 'here comes great news.'], check=False)
-  process = subprocess.run(['git', 'push'], check=False)
-  os.chdir(old_dir)
-
+old_dir = os.getcwd()
+os.chdir(r'public/blog')
+process = subprocess.run(['git', 'add', '--all'], check=False)
+process = subprocess.run(['git', 'commit', '-m', 'here comes great news.'], check=False)
+process = subprocess.run(['git', 'push'], check=False)
+os.chdir(old_dir)
