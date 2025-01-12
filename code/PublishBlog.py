d28f0e5c72b010d204454638649a9b5aa496b2e5
Wanderley Caloni <wanderley.caloni@gmail.com>
2025-01-12 15:28:40 -0300

    Modify default commit messages to simple standard

--- code/PublishBlog.py
+++ code/PublishBlog.py
@@ -13,12 +13,12 @@ if process.returncode:
   print('blogbuild.py returned', process.returncode)
 
 process = subprocess.run(['git', 'add', '--all'], check=False)
-process = subprocess.run(['git', 'commit', '-m', 'publishing some shit.'], check=False)
+process = subprocess.run(['git', 'commit', '-m', 'Add journal changes'], check=False)
 process = subprocess.run(['git', 'push'], check=False)
 
 old_dir = os.getcwd()
 os.chdir(r'public/blog')
 process = subprocess.run(['git', 'add', '--all'], check=False)
-process = subprocess.run(['git', 'commit', '-m', 'here comes great news.'], check=False)
+process = subprocess.run(['git', 'commit', '-m', 'Publish lastest posts'], check=False)
 process = subprocess.run(['git', 'push'], check=False)
 os.chdir(old_dir)
