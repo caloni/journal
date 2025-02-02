e047d17be2667e7c916d479c0cedf9828bd48a49
Wanderley Caloni <wanderley.caloni@gmail.com>
2025-02-01 22:06:38 -0300

    Add journal changes

--- code/movies.py
+++ code/movies.py
@@ -43,6 +43,25 @@ def print_desc(imdb, args):
     print(desc)
 
 
+def print_short_desc(imdb):
+    ia = IMDb()
+    movie = ia.get_movie(imdb)
+    originalTitle = movie["title"]
+    
+    def getItem(name, movie, itemMax = 10):
+        ret = None
+        if name in movie:
+            ret = movie[name][0:itemMax]
+            ret = str(ret[0]) if len(ret) == 1 else (", ".join(map(str, ret[0:-1])) + " e " + str(ret[-1]))
+        return ret
+
+    director = getItem("director", movie)
+    year = str(movie["year"])
+
+    desc = "(" + originalTitle + ", " + director + ", " + year + ")"
+    print(desc)
+
+
 def save_database(imdb, path):
     ignore_keys = [ 'synopsis', 'plot', 'writer', 'director', 'miscellaneous' ]
     f = open(path, 'w', encoding='utf8')
@@ -65,7 +84,7 @@ def search_movie(query):
     ia = IMDb()
     movies = ia.search_movie(query)
     results = []
-    for m in movies[0:5]:
+    for m in movies[0:1]:
         plot = ia.get_movie_plot(m.movieID)
         if 'plot' in plot['data']:
             plot = plot['data']['plot'][0][0:140]
@@ -74,8 +93,8 @@ def search_movie(query):
         results.append({ 'id': str(m.movieID), 'title': str(m), 'plot': plot})
     for r in results:
         print(r)
-    opt = input('what is the movie? ')
-    return results[int(opt)]['id']
+    #opt = input('what is the movie? ')
+    #return results[int(opt)]['id']
 
 
 if len(sys.argv) < 2:
@@ -89,8 +108,11 @@ else:
     if sys.argv[1] == 'cinemaqui':
         print_desc(sys.argv[2], sys.argv[3:])
     elif sys.argv[1] == 'search':
-        imdb = search_movie(sys.argv[2].replace('-', ' '))
-        save_database(imdb, sys.argv[3])
+        search_movie(sys.argv[2].replace('-', ' '))
+        #imdb = search_movie(sys.argv[2].replace('-', ' '))
+        #save_database(imdb, sys.argv[3])
+    elif sys.argv[1] == 'review':
+        print_short_desc(sys.argv[2])
     else:
         save_database(sys.argv[1], sys.argv[2])
 
