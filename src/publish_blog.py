817e4696b7221a46ac69d4a3c7b74f0e9e5d36c8
Wanderley Caloni <wanderley.caloni@gmail.com>
2025-05-01 22:27:00 -0300

    Ignore .git folder to daily backup

--- src/publish_blog.py
+++ src/publish_blog.py
@@ -3,6 +3,9 @@ import subprocess
 import shutil
 from datetime import datetime
 
+def ignore_git(dir, files):
+    return ['.git'] if '.git' in files else []
+
 backup = True
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
@@ -14,12 +17,13 @@ if( backup ):
   if not os.path.exists(backup_dir):
     os.makedirs(backup_dir)
   backup_path = os.path.join(backup_dir, 'journal.zip')
-  print(f'creating backup at {backup_path}')
   shutil.make_archive(
     os.path.join(backup_dir, 'journal'),
     'zip',
-    os.getcwd())
-  print(f'backup created')
+    os.getcwd(),
+    None,
+    ignore_git)
+  print(f'backup created at {backup_path}')
 
 process = subprocess.run(['python', os.path.join(dname, 'Journal2Blog.py')], check=True)
 if process.stdout:
